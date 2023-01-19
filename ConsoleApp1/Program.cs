01f53bba70fcb262d459ff2c0a7cbee138717afa
kelseyjoshuaphillips <phil0382@algonquinlive.com>
2023-01-19 14:28:30 -0500

    Added menu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void main (string[] args)
+        static void Main(string[] args)
         {
             //Choose an option
             //1 - Import Words from File
@@ -17,13 +17,55 @@ namespace ConsoleApp1
             //8 - Get and display words that contain 'q' and display the count
             //9 - Get and display words that are more than 3 characters long and start with the letter 'a', and display the count
             //x - Exit
+            while (true)
+            {
+                Console.WriteLine(
+                    "1 - Import Words from File\n" +
+                    "2 - Bubble Sort words\n" +
+                    "3 - LINQ/Lambda sort words\n" +
+                    "4 - Count the Distinct Words\n" +
+                    "5 - Take the last 10 words\n" +
+                    "6 - Reverse print the words\n" +
+                    "7 - Get and display words that end with 'd' and display the count\n" +
+                    "8 - Get and display words that contain 'q' and display the count\n" +
+                    "9 - Get and display words that are more than 3 characters long and start with the letter 'a', and display the count\n" +
+                    "x - Exit");
 
+                string i = Console.ReadLine();
 
+                switch (i)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        break;
+                    case "8":
+                        break;
+                    case "9":
+                        break;
+                    case "x":
+                        Console.WriteLine("Goodbye");
+                        Thread.Sleep(100);
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid selection, try again");
+                        break;
+                }
 
+            }
 
         }
-
-
 
 
     }
