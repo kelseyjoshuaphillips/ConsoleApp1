a03c71ec604fab13d7deae953d76e05490b40d79
kelseyjoshuaphillips <phil0382@algonquinlive.com>
2023-01-19 14:02:37 -0500

    Wrote main menu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void main (string[] args)
+        {
+            //Choose an option
+            //1 - Import Words from File
+            //2 - Bubble Sort words
+            //3 - LINQ/Lambda sort words
+            //4 - Count the Distinct Words
+            //5 - Take the last 10 words
+            //6 - Reverse print the words
+            //7 - Get and display words that end with 'd' and display the count
+            //8 - Get and display words that contain 'q' and display the count
+            //9 - Get and display words that are more than 3 characters long and start with the letter 'a', and display the count
+            //x - Exit
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+}
+
+
