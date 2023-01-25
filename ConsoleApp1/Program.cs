6097c4fa9f41f0b6f5908522b78f4acd25b85fdf
kelseyjoshuaphillips <phil0382@algonquinlive.com>
2023-01-24 19:16:36 -0500

    Fixed bug and complete

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Console Program Lab 1
+ * CST8359 302
+ * By: Kelsey Phillips 
+ */
+using System;
 using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Xml.Serialization;
@@ -11,21 +16,11 @@ namespace ConsoleApp1
         private IList<string> words = new List<string>();
         static void Main(string[] args)
         {
+            //Program object
             Program x = new Program();
-            //Choose an option
-            //1 - Import Words from File
-            //2 - Bubble Sort words
-            //3 - LINQ/Lambda sort words
-            //4 - Count the Distinct Words
-            //5 - Take the last 10 words
-            //6 - Reverse print the words
-            //7 - Get and display words that end with 'd' and display the count
-            //8 - Get and display words that contain 'q' and display the count
-            //9 - Get and display words that are more than 3 characters long and start with the letter 'a', and display the count
-            //x - Exit
+
             while (true)
             {
-                //test
                 Console.WriteLine(
                     "1 - Import Words from File\n" +
                     "2 - Bubble Sort words\n" +
@@ -37,10 +32,10 @@ namespace ConsoleApp1
                     "8 - Get and display words that contain 'q' and display the count\n" +
                     "9 - Get and display words that are more than 3 characters long and start with the letter 'a', and display the count\n" +
                     "x - Exit");
-
+                //Get input from user
                 string i = Console.ReadLine();
                
-
+                //Selection
                 switch (i)
                 {
                     //Imports Words from File
@@ -135,7 +130,7 @@ namespace ConsoleApp1
 
         }
 
-
+        //|..//..//..//..//words.txt
 
         //Import words from a text file and return the number of words
         private void Import()
@@ -150,22 +145,22 @@ namespace ConsoleApp1
             Console.WriteLine("There are " + words.Count() + " words.");
         }
 
+        //Sort by bubblesort
         private IList<string> BubbleSort(IList<string> words)
         {
+            var temp = words.ToList();
             var watch = new System.Diagnostics.Stopwatch();
-            string temp ="";
+            string temp2 ="";
             watch.Start();
-            for (int i = 0; i < words.Count; i++)
+            for (int i = 0; i < temp.Count; i++)
             {
-                for (int j = 0; j < words.Count-1; j++)
+                for (int j = 0; j < temp.Count-1; j++)
                 {
-                    //if (string.Compare(words[j], words[j+1]) > 0)
-                    //if (words[j].Length > words[j + 1].Length)
-                    if (words[j].CompareTo(words[j+1]) > 0)
+                    if (temp[j].CompareTo(temp[j+1]) > 0)
                     {
-                        temp = words[j + 1];
-                        words[j + 1] = words[j];
-                        words[j] = temp;
+                        temp2 = temp[j + 1];
+                        temp[j + 1] = temp[j];
+                        temp[j] = temp2;
                     }
                 }
             }
@@ -173,29 +168,28 @@ namespace ConsoleApp1
             var time = watch.ElapsedMilliseconds;
 
             Console.WriteLine("Time elapsed: " + time + "ms");
-            return words;
+            return temp;
         }
 
+        //Sort using Linq
         private IList<string> LINQSort(IList<string> words)
         {
             var watch = new System.Diagnostics.Stopwatch();
             watch.Start();
-            words = words.OrderBy(x => x).ToList();
+            var temp = words.OrderBy(x => x).ToList();
             watch.Stop();
             var time = watch.ElapsedMilliseconds;
             Console.WriteLine("Time elapsed: " + time + "ms");
-            return words;
+            return temp;
         }
+        //Find distinct words with Linq
         private IList<string> LINQDistinct (IList<string> words)
         {
-            //words = words.Distinct().OrderBy(x => x).ToList();
-            //var blah = words.Take(10);
-            //foreach(string word in blah)
-            //Console.WriteLine(word);
             int count = words.Distinct().ToList().Count();
             Console.WriteLine("Distinct count is " + count);
             return words;
         }
+        //Get 10 last words
         private IList<string> LINQLast10(IList<string> words)
         {
             words = words.Reverse().Take(10).Reverse().ToList();
@@ -203,21 +197,23 @@ namespace ConsoleApp1
                 Console.WriteLine(word); 
             return words;
         }
+        //Reverse print list
         private void ReversePrint(IList<string> words)
         {
             words = words.Reverse().ToList();
             foreach (String word in words)
                 Console.WriteLine(word);
         }
+        //Words that end in D
         private void EndWithD(IList<string> words)
         {
             words = words.Where(x => x.EndsWith('d')).ToList();
             int count = words.Count();
             Console.WriteLine("The " + count + " words that end in 'd' are;");
             foreach (String word in words)
-                Console.WriteLine(word);
-            
+                Console.WriteLine(word);        
         }
+        //Words that have Q
         private void ContainsQ(IList<string> words)
         {
             words = words.Where(x => x.Contains('q')).ToList();
@@ -225,8 +221,8 @@ namespace ConsoleApp1
             Console.WriteLine("The " + count + " words that contain 'q' are; ");
             foreach (String word in words)
                 Console.WriteLine(word);
-
         }
+        //Words that are longer than 3 characters and have an 'a'
         private void MoreThan3Long(IList<string> words)
         {
             words = words.Where(x => x.Length > 3 && x.StartsWith('a')).ToList();
